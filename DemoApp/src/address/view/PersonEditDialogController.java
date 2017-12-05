2cdd6b3dddf99f3b078d685cd3c395d92fa78687
SiriusSee <SiriusSee@outlook.com>
2017-12-05 18:22:44 +0800

    add demo-project

---
+++ DemoApp/src/address/view/PersonEditDialogController.java
@@ -0,0 +1,52 @@
+package address.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import org.controlsfx.dialog.Dialogs;
+
+import address.model.Person;
+import address.util.DateUtil;
+
+/**
+ * Dialog to edit details of a person.
+ * 
+ * @author Marco Jakob
+ */
+public class PersonEditDialogController {
+
+	@FXML
+	private TextField firstNameField;
+	@FXML
+	private TextField lastNameField;
+	@FXML
+	private TextField streetField;
+	@FXML
+	private TextField postalCodeField;
+	@FXML
+	private TextField cityField;
+	@FXML
+	private TextField birthdayField;
+
+	private Stage dialogStage;
+	private Person person;
+	private boolean okClicked = false;
+
+	/**
+	 * Initializes the controller class. This method is automatically called after
+	 * the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+	}
+
+	/**
+	 * Sets the stage of this dialog.
+	 * 
+	 * @param dialogStage
+	 */
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+}
